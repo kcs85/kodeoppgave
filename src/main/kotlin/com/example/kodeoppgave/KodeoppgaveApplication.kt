9967ec088e8aa03e86eaf24f9851bb052346d8d2
Kristina Skåtun <kristinaskatun@gmail.com>
2021-06-22 13:25:19 +0200

    Opprett backend med mvn og spring boot

---
+++ src/main/kotlin/com/example/kodeoppgave/KodeoppgaveApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kodeoppgave
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KodeoppgaveApplication
+
+fun main(args: Array<String>) {
+    runApplication<KodeoppgaveApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kodeoppgave/KodeoppgaveApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kodeoppgave
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KodeoppgaveApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
